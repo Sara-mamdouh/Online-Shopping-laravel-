df7ab45940b3ebbe432d1664108452ca30af804c
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-03-25 14:38:23 +0200

    some change

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,7 +10,7 @@
   <table class="table">
     <thead>
       <tr>
-        <th scope="col"># ID testttttt</th>
+        <th scope="col">#ID</th>
         <th scope="col">Name</th>
         <th scope="col">Price</th>
         <th scope="col">created_at</th>
