5da81e8fc2c6476d05cbaf0c43bf0dfb500e4ae7
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-03-25 16:21:05 +0200

    display error message for each input

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,15 +3,7 @@
 <title> create</title>
 @endsection
 @section("content")
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+
 <form action="{{ route("products.store")}}" method="post" class="container mt-4">
   @csrf
   {{-- <div class="mb-3">
@@ -22,10 +14,20 @@
     <label for="product_name" class="form-label">name</label>
     <input type="text" class="form-control" id="product_name" name="name">
   </div>
+  @error("name")
+    <div class="alert alert-danger">
+      {{$message}}
+    </div>
+  @enderror
   <div class="mb-3">
     <label for="product_price" class="form-label">price</label>
     <input type="text" class="form-control" id="product_price" name="price">
   </div>
+  @error("price")
+    <div class="alert alert-danger">
+      {{$message}}
+    </div>
+  @enderror
   
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
